7ec525831a33805ed40caaf4a9c8386f39db83fd
Stenar Tikk <stenar.tikk29@gmail.com>
2025-06-05 18:28:52 +0300

    fix: add routes to fix & display API documentation in production

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return config('app.name');
-});
+Route::redirect('/', 'api/documentation');
+Route::get('/docs', function () {
+    return response()->file(storage_path('api-docs/api-docs.json'), [
+        'Content-Type' => 'application/json',
+    ]);
+})->name('l5-swagger.default.docs');
