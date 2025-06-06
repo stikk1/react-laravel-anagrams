f09235d9adf1516fe0c403838b24aa2490749170
Stenar Tikk <stenar.tikk29@gmail.com>
2025-06-06 13:59:06 +0300

    chore: publish React build output to Laravel public folder

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', 'api/documentation');
 Route::get('/docs', function () {
     return response()->file(storage_path('api-docs/api-docs.json'), [
         'Content-Type' => 'application/json',
     ]);
 })->name('l5-swagger.default.docs');
+
+Route::get('/{any}', function () {
+    $index = public_path('index.html');
+
+    return Response::file($index);
+})->where('any', '.*');
